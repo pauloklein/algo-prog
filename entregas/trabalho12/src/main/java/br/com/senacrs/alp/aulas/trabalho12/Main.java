9c51b644de572fcc025d6a24660c367ec17bca0c
Paulo <Paulo@p-210eac05f0064>
2012-11-11 23:48:10 -0200

    Implementação de leitura de parâmetros de arquivos de Configuração e Requisição de webserver.

--- entregas/trabalho12/src/main/java/br/com/senacrs/alp/aulas/trabalho12/Main.java
+++ entregas/trabalho12/src/main/java/br/com/senacrs/alp/aulas/trabalho12/Main.java
@@ -10,71 +10,234 @@ import java.util.Map;
 
 public class Main {
 
-	private static File arquivo = null;
-	private final static String NOME_INDEX = "index.html";
-			
 	public static void main(String[] args) {
+		
+		String nomeArquivoConfiguracao = null;
+		String nomeArquivoRequisicao = null;
+		
+		Map<String, String> hashConfiguracao = new HashMap<String, String>();
+		Map<String, String> hashRequisicao = new HashMap<String, String>();
+		
+		String caminhoRelativo = null;
+		String caminhoAbsoluto = null;
+		
+		String host = null;
+		String get = null;
+		
+		if(args == null){
+			System.out.println("ERRO");
+			throw new IllegalArgumentException();
+		} else {
+			if(args.length == 1){
+				nomeArquivoConfiguracao = args[0];
+			} else if(args.length == 2){
+				nomeArquivoConfiguracao = args[0];
+				nomeArquivoRequisicao = args[1];
+			}
+			
+			if(nomeArquivoConfiguracao != null){
+				hashConfiguracao = validaArquivoConfiguracao(nomeArquivoConfiguracao);
+				caminhoRelativo = hashConfiguracao.get("root_dir").replace(".",System.getProperty("user.dir")).replace('/', File.separatorChar);
+				caminhoAbsoluto = caminhoRelativo.replace(".",System.getProperty("user.dir"));
+				
+				System.out.println(hashConfiguracao.get("port") + ":" + caminhoAbsoluto);
+				
+				if(nomeArquivoRequisicao != null){
+					hashRequisicao = validaArquivoRequisicao(nomeArquivoRequisicao);
+					host = hashRequisicao.get("Host:");
+					get = hashRequisicao.get("GET").split(" ")[0];
+					
+					System.out.println(host + get);
+				}				
+			}			
+		}			
+	}
+
+	private static Map<String, String> validaArquivoConfiguracao(String nomeArquivo) {
+		
+		File arquivo = null;
+		
+		String[] par = new String[2];
+		Map<String, String> hash = new HashMap<String, String>();
 		
 		FileReader reader = null;
 		BufferedReader input = null;
 		String line = null;
 		
-		String[] par = new String[2];
-		Map<String, String> hash = new HashMap<String, String>();
-				
-		if(args == null){
+		String caminhoRelativo = null;
+		
+		arquivo = new File(nomeArquivo);
+		if (!arquivo.exists() || arquivo.isDirectory()) {
 			System.out.println("ERRO");
 			throw new IllegalArgumentException();
 		} else {
-			String nomeArquivo = args[0];
-			arquivo = new File(nomeArquivo);
-			if (!arquivo.exists() || arquivo.isDirectory()) {
-				System.out.println("ERRO");
-				throw new IllegalArgumentException();
-			} else {
+			try {
+				reader = new FileReader(arquivo);
 				try {
-					reader = new FileReader(arquivo);
-					try {
-						input = new BufferedReader(reader);
-						line = input.readLine();
+					input = new BufferedReader(reader);
+					line = input.readLine();
 
-						while (line != null) {
-							int total = 0;
-							for (int i = 0; i < line.length(); i++) {
-								if (line.charAt(i) == '=') {
-									total++;
-								}
+					while (line != null) {
+						int total = 0;
+						for (int i = 0; i < line.length(); i++) {
+							if (line.charAt(i) == '=') {
+								total++;
 							}
-							if (total != 1) {
-								System.out.println("ERRO");
-								input.close();
-								throw new IllegalArgumentException();
-							}
-							
-							par = line.split("=");
-							hash.put(par[0].trim(), par[1].trim());
-							line = input.readLine();							
 						}
-						
-						if(!hash.containsKey("root_dir") || !hash.containsKey("port")){
+						if (total != 1) {
 							input.close();
 							System.out.println("ERRO");
 							throw new IllegalArgumentException();
 						}
 						
-						System.out.println(hash.get("port") + ":" + hash.get("root_dir").replace("/", "\\") + NOME_INDEX);
-						
-						input.close();
-					} catch (IOException e2) {
+						par = line.split("=");
+						hash.put(par[0].trim(), par[1].trim());
+						line = input.readLine();							
+					}					
+					input.close();
+					
+					if(!hash.containsKey("root_dir") || !hash.containsKey("port")){
 						System.out.println("ERRO");
-						throw new IllegalArgumentException(e2);
+						throw new IllegalArgumentException();
+					} 
+					
+					try{
+						Integer.parseInt(hash.get("port"));
+					} catch(Exception e3){
+						System.out.println("ERRO");
+						throw new IllegalArgumentException(e3);
 					}
-				} catch (FileNotFoundException e1) {
+					
+					if(hash.get("port").length() > 5){
+						System.out.println("ERRO");
+						throw new IllegalArgumentException();
+					}
+					
+					caminhoRelativo = hash.get("root_dir").replace(".",System.getProperty("user.dir")).replace('/', File.separatorChar);
+					arquivo = new File(caminhoRelativo);
+					
+					if(!arquivo.isDirectory() || !arquivo.exists()){
+						System.out.println("ERRO");
+						throw new IllegalArgumentException();
+					}
+					
+				} catch (IOException e2) {
 					System.out.println("ERRO");
-					throw new IllegalArgumentException(e1);
+					throw new IllegalArgumentException(e2);
 				}
+			} catch (FileNotFoundException e1) {
+				System.out.println("ERRO");
+				throw new IllegalArgumentException(e1);
 			}
-		}		
+		}
+		
+		return hash;
+	}		
+	
+	private static Map<String, String> validaArquivoRequisicao(String nomeArquivo) {
+		File arquivo = null;
+		
+		String[] parametros = null;
+		Map<String, String> hash = new HashMap<String, String>();
+		
+		FileReader reader = null;
+		BufferedReader input = null;
+		String line = null;
+		
+		arquivo = new File(nomeArquivo);
+		if (!arquivo.exists() || arquivo.isDirectory()) {
+			System.out.println("ERRO");
+			throw new IllegalArgumentException();
+		} else {
+			try {
+				reader = new FileReader(arquivo);
+				try {
+					input = new BufferedReader(reader);
+					line = input.readLine();
+
+					while (line != null) {
+						line = line.trim();
+						
+						int total = 0;
+						for (int i = 0; i < line.length(); i++) {
+							if (line.charAt(i) == ' ') {
+								total++;
+							}
+						}
+						if (total == 2) {
+							parametros = new String[3];
+							parametros = line.split(" ");
+							hash.put(parametros[0].trim(), parametros[1].trim() + " " + parametros[2].trim());
+						} else if(total == 1) {
+							parametros = new String[2];
+							parametros = line.split(" ");
+							hash.put(parametros[0].trim(), parametros[1].trim());
+						} else {
+							input.close();
+							System.out.println("ERRO");
+							throw new IllegalArgumentException();
+						}
+						
+						line = input.readLine();							
+					}
+					input.close();
+					
+					if(!hash.containsKey("GET") || !hash.containsKey("Host:")){
+						System.out.println("ERRO");
+						throw new IllegalArgumentException();
+					} 
+					
+					String[] get = new String[2];
+					get = hash.get("GET").split(" ");
+					
+					String path = get[0];
+								
+					int total = 0;
+					int posicao = -1;
+					for (int i = 0; i < path.length(); i++) {
+						if (path.charAt(i) == '/') {
+							if(i == 0){
+								posicao = i;
+							}
+							total++;
+						}
+					}
+					if (posicao != 0 || total < 1) {
+						System.out.println("ERRO");
+						throw new IllegalArgumentException();
+					}
+					
+					String protocol = get[1];
+					
+					if(!protocol.equalsIgnoreCase("http/1.1")){
+						System.out.println("ERRO");
+						throw new IllegalArgumentException();
+					}
+					
+					String host = hash.get("Host:");
+					total = 0;
+					for (int i = 0; i < host.length(); i++) {
+						if (host.charAt(i) == '.') {
+							total++;
+						}
+					}
+					if (total < 2) {
+						System.out.println("ERRO");
+						throw new IllegalArgumentException();
+					}
+						
+				} catch (IOException e2) {
+					System.out.println("ERRO");
+					throw new IllegalArgumentException(e2);
+				}
+			} catch (FileNotFoundException e1) {
+				System.out.println("ERRO");
+				throw new IllegalArgumentException(e1);
+			}
+		}
+		
+		return hash;
 		
 	}
+
 }
--- entregas/trabalho12/src/test/java/br/com/senacrs/alp/aulas/trabalho12/MainTest.java
+++ entregas/trabalho12/src/test/java/br/com/senacrs/alp/aulas/trabalho12/MainTest.java
@@ -15,30 +15,24 @@ public class MainTest {
 			+ File.separatorChar
 			+ "configuracoes"
 			+ File.separatorChar;
-	private final static String NOME_ARQUIVO_CORRETO = "configuracoes_correto.txt";
-	private final static String ARQUIVO_CORRETO = DIRETORIO
-			+ NOME_ARQUIVO_CORRETO;
-	private final static String NOME_ARQUIVO_INCORRETO = "configuracoes_incorreto.txt";
-	private final static String ARQUIVO_INCORRETO = DIRETORIO
-			+ NOME_ARQUIVO_INCORRETO;
-	
-	public void criarMain(String argumento) {
-		
-		String[] args = null;
-		
-		if(argumento != null){
-			args = new String[1];
-			args[0] = argumento;
-		}
-		
-		Main.main(args);
-	}
+	private final static String NOME_ARQUIVO_CONFIG_CORRETO = "config_correto.prop";
+	private final static String ARQUIVO_CONFIG_CORRETO = DIRETORIO
+			+ NOME_ARQUIVO_CONFIG_CORRETO;
+	private final static String NOME_ARQUIVO_CONFIG_INCORRETO = "config_incorreto.prop";
+	private final static String ARQUIVO_CONFIG_INCORRETO = DIRETORIO
+			+ NOME_ARQUIVO_CONFIG_INCORRETO;
+	private final static String NOME_ARQUIVO_REQ_CORRETO = "req_correto.txt";
+	private final static String ARQUIVO_REQ_CORRETO = DIRETORIO
+			+ NOME_ARQUIVO_REQ_CORRETO;
+	private final static String NOME_ARQUIVO_REQ_INCORRETO = "req_incorreto.txt";
+	private final static String ARQUIVO_REQ_INCORRETO = DIRETORIO
+			+ NOME_ARQUIVO_REQ_INCORRETO;
 	
 	@Test
-	public void testeArquivoNull() {
+	public void testeArquivosNull() {
 		
 		try {
-			criarMain(null);
+			Main.main(null);
 			fail("Deveria ter abortado.");
 		} catch (IllegalArgumentException e) {
 			Assert.assertTrue(true);
@@ -46,10 +40,13 @@ public class MainTest {
 	}
 	
 	@Test
-	public void testeArquivoDiretorio() {
+	public void testeArquivoConfigDiretorio() {
+		
+		String[] args = new String[1];
+		args[0] = DIRETORIO;
 		
 		try {
-			criarMain(DIRETORIO);
+			Main.main(args);
 			fail("Deveria ter abortado.");
 		} catch (IllegalArgumentException e) {
 			Assert.assertTrue(true);
@@ -58,10 +55,13 @@ public class MainTest {
 	}
 	
 	@Test
-	public void testArquivoInexistente() {
+	public void testeArquivoConfigInexistente() {
 
+		String[] args = new String[1];
+		args[0] = "NaoExiste" + ".nao_existe";
+		
 		try {
-			criarMain("NaoExiste" + ".nao_existe");
+			Main.main(args);
 			fail("Deveria ter abortado");
 		} catch (IllegalArgumentException e) {
 			Assert.assertTrue(true);
@@ -69,10 +69,13 @@ public class MainTest {
 	}	
 	
 	@Test
-	public void testArquivoIncorreto() {
+	public void testeArquivoConfigIncorreto() {
+		
+		String[] args = new String[1];
+		args[0] = ARQUIVO_CONFIG_INCORRETO;
 		
 		try {
-			criarMain(ARQUIVO_INCORRETO);
+			Main.main(args);
 			fail("Deveria ter abortado.");
 		} catch (Exception e) {
 			Assert.assertTrue(true);
@@ -80,13 +83,78 @@ public class MainTest {
 	}	
 
 	@Test
-	public void testArquivoCorreto() {
+	public void testeArquivoConfigCorreto() {
+		
+		String[] args = new String[1];
+		args[0] = ARQUIVO_CONFIG_CORRETO;
 		
 		try {
-			criarMain(ARQUIVO_CORRETO);
+			Main.main(args);
 			Assert.assertTrue(true);
 		} catch (Exception e) {
 			fail("Deveria ter passado.");			
 		}		
 	}	
+	
+	@Test
+	public void testeArquivoReqDiretorio() {
+		
+		String[] args = new String[2];
+		args[0] = ARQUIVO_CONFIG_CORRETO;
+		args[1] = DIRETORIO;
+		
+		try {
+			Main.main(args);
+			fail("Deveria ter abortado.");
+		} catch (IllegalArgumentException e) {
+			Assert.assertTrue(true);
+		}		
+		
+	}
+	
+	@Test
+	public void testeArquivoReqInexistente() {
+
+		String[] args = new String[2];
+		args[0] = ARQUIVO_CONFIG_CORRETO;
+		args[1] = "NaoExiste" + ".nao_existe";
+		
+		try {
+			Main.main(args);
+			fail("Deveria ter abortado");
+		} catch (IllegalArgumentException e) {
+			Assert.assertTrue(true);
+		}
+	}	
+	
+	@Test
+	public void testeArquivoReqIncorreto() {
+		
+		String[] args = new String[2];
+		args[0] = ARQUIVO_CONFIG_CORRETO;
+		args[1] = ARQUIVO_REQ_INCORRETO;
+		
+		try {
+			Main.main(args);
+			fail("Deveria ter abortado.");
+		} catch (Exception e) {
+			Assert.assertTrue(true);	
+		}		
+	}	
+	
+	@Test
+	public void testeArquivoReqCorreto() {
+		
+		String[] args = new String[2];
+		args[0] = ARQUIVO_CONFIG_CORRETO;
+		args[1] = ARQUIVO_REQ_CORRETO;
+		
+		try {
+			Main.main(args);
+			Assert.assertTrue(true);
+		} catch (Exception e) {
+			fail("Deveria ter passado.");			
+		}		
+	}	
+	
 }
